35af58515090cfc1f1eeefe204428f0fa1423223
Kingsley Boahen <kingsleyboahen34@gmail.com>
2022-08-23 01:42:41 -0500

    initial commit

---
+++ lib/screens/all_movies_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:movie_app/screens/movie_details_screen.dart';
+
+class AllMoviesScreen extends StatelessWidget {
+  const AllMoviesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffE8E7E7),
+      appBar: allmoviesApp(context),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+         return GestureDetector(
+          child: MovieCard(),
+          onTap: () {
+
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return MovieDetailsScreen(CurrentIndex: index,);
+                  },
+                ),
+              );
+            },
+          ); 
+        }
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  AppBar allmoviesApp(BuildContext context) {
+    return AppBar(title: Text(
+      "Timeline",
+      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+             fontSize: 20,
+             fontWeight: FontWeight.bold,
+           ),
+      ),
+    backgroundColor: Colors.grey.withOpacity(0.5),
+    centerTitle: true,
+    );
+  }
+}
+
+class MovieCard extends StatelessWidget {
+  const MovieCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 2.5,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const[
+                  Text("Release Date"),
+                  Icon(Icons.more_horiz),
+                ],
+              ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 10,
+                    spreadRadius: 5,
+                    offset: const Offset(0, 5),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(FeatherIcons.heart),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(FeatherIcons.messageCircle),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(FeatherIcons.cornerUpRight),
+                      ),
+                     ],
+                   ),
+                  Row(
+                     children: const [
+                      Icon(Icons.star),
+                      Text("4.5"),
+                     ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/movie_details_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class MovieDetailsScreen extends StatelessWidget {
+   MovieDetailsScreen({Key? key,this.CurrentIndex}) : super(key: key);
+
+   int? CurrentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Card(
+              margin: const EdgeInsets.only(left: 0, right: 0, top: 0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 1.7,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+              color: Colors.grey,
+                image: DecorationImage(
+                  image: NetworkImage("https://www.omdbapi.com/src/poster.jpg"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                        onPressed: () => Navigator.pop(context), 
+                        icon: Icon(Icons.arrow_back_ios)),
+                    ),
+                    
+                    Container(
+                      height: 50,
+                      color: Colors.black.withOpacity(0.3),
+                       child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Material(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Icon(FeatherIcons.playCircle),
+                              ),
+                            ),
+                          )
+                        ],
+                       ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+           Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Wrap(
+                children: [
+                  Text(
+                    "Description:",
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 20, fontWeight: FontWeight.bold), 
+                    ),
+                 ],
+                ),
+                  
+                 Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    Text("Director:",
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                 ),
+
+                 Wrap(
+                  children: [
+                    Text("Producer:",
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                 ),
+              ],
+          ),
+            ))
+        ],
+      ),
+    );
+  }
+}
